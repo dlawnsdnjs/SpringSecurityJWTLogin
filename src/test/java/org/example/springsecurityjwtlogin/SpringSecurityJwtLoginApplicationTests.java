a0b1a8c309ed7899411b832ca77844abeefc6b45
dlawnsdnjs <59200605+dlawnsdnjs@users.noreply.github.com>
2025-03-19 16:25:41 +0900

    project create

---
+++ src/test/java/org/example/springsecurityjwtlogin/SpringSecurityJwtLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityjwtlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
