0297da1f0c7fb1f2415d0bc2d390baf18d12d276
dlawnsdnjs <59200605+dlawnsdnjs@users.noreply.github.com>
2025-03-19 17:02:45 +0900

    user entity

---
+++ src/main/java/org/example/springsecurityjwtlogin/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.example.springsecurityjwtlogin.Repository;
+
+import org.example.springsecurityjwtlogin.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
